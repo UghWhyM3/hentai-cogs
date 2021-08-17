6d40dbf681c2145ca2b1aaebed377660f3e0d604
UghWhyM3 <89093642+UghWhyM3@users.noreply.github.com>
2021-08-17 12:24:13 -0400

    Forgot ctx

--- hentai/hentai.py
+++ hentai/hentai.py
@@ -24,7 +24,7 @@ class Hentai(commands.Cog):
             "https://cdn.discordapp.com/attachments/872703562168291362/877037988553572412/sample_ff73b52e844f77b41f27529bbddbbd24.png",
             "https://cdn.discordapp.com/attachments/872703562168291362/876978349975695390/8jcrubk2yph71.png"
         ]
-        await self.maybe_send_embed("Hentai!", random.choice(images))
+        await self.maybe_send_embed(ctx, "Hentai!", random.choice(images))
 
     async def maybe_send_embed(self, ctx: commands.Context, title: str, url: str):
         kwargs = {"content": f"**{title}**\n\n{url}"}
